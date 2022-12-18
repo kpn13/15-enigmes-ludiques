c997ca71b7d1042ff2244a50c21d6600278188f9
Karim Pinchon <karim.pinchon@meero.com>
2022-12-18 20:55:47 +0100

    Succeed enigme 1

---
+++ src/enigme1.py
@@ -0,0 +1,8 @@
+with open("../data/Enigme1.txt", "r") as f:
+    ascii_string = f.read()
+
+ascii_codes = ascii_string.split(" ")
+
+decoded_string = "".join(chr(int(code)) for code in ascii_codes)
+
+print(decoded_string)
